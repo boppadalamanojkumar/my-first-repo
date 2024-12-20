8f27817b562a3f4ac399f7ed009a56e840f3378a
boppadalamanojkumar <boppadalamanojkumar20.com>
2024-12-20 12:44:26 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Products from './components/products'
+import EmpSal from './components/Empsal'
+import EmpApi from './components/EmpApi'
+export default function App() {
+  return (
+    <div><Products/><EmpSal/><EmpApi/></div>
+  )
+}
